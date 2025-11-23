d1fc095009cc12171997e1d187ccea5d7c267dad
davidapraise <davidapraise@gmail.com>
2025-11-23 19:47:45 +0000

    Update 01_daily_sales_trends.sql

--- sql/01_daily_sales_trends.sql
+++ sql/01_daily_sales_trends.sql
@@ -1,1 +1,6 @@
-
+SELECT 
+  EXTRACT(HOUR FROM datetime) AS hours,
+  COUNT(*) AS coffee_sales
+FROM `sql-exploration-analysis.coffee_sales_ukraine.coffee_sales`
+GROUP BY hours
+ORDER BY coffee_sales DESC;
