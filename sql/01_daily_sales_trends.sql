152ae5aee32bccd01db4151d97a7f181b10cd730
davidapraise <davidapraise@gmail.com>
2025-11-23 20:21:54 +0000

    Change order of coffee sales results to ascending

--- sql/01_daily_sales_trends.sql
+++ sql/01_daily_sales_trends.sql
@@ -1,6 +1,10 @@
+#THE TOTAL SALES OF COFFEE BY EACH HOUR OF THE DAY#
+#COLUMNS USED: HOURS, COFFEE_SALES#
+#SHOWS WHICH HOUR OF THE DAY HAS HAD THE MOST SALES#
+
 SELECT 
   EXTRACT(HOUR FROM datetime) AS hours,
   COUNT(*) AS coffee_sales
 FROM `sql-exploration-analysis.coffee_sales_ukraine.coffee_sales`
 GROUP BY hours
-ORDER BY coffee_sales DESC;
+ORDER BY hours ASC;
